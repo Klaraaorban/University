3fc87d80d97a8c279831c2cf412b244261600693
Klaraaorban <klara.orban@stud.ubbcluj.ro>
2024-10-13 14:03:37 +0300

    aufgabe 3 beendet

--- src/lab1/Main1.java
+++ src/lab1/Main1.java
@@ -1,8 +1,12 @@
 package lab1;
 
+import java.util.Arrays;
+
 public class Main1 {
     public static void main(String[] args) {
-        /*int[] note = {29, 37, 38, 41, 84, 67, 68};
+        /*
+        //Aufgabe 1
+        int[] note = {29, 37, 38, 41, 84, 67, 68};
         Note noteCalc = new Note(note);
 
         System.out.println("Nicht ausreichender Note: " + noteCalc.nichtAusreichenderNote());
@@ -13,12 +17,29 @@ public class Main1 {
 
         System.out.println("Maximal Abgerundete Note: " + noteCalc.maxAbgerundeteNote());*/
 
+        /*
+        //Aufgabe 2
         int[] numbers = {4, 8, 3, 10, 17};
         Zahlen nums = new Zahlen(numbers);
 
         System.out.println("Maximale Zahl: " + nums.maximale());
         System.out.println("Minimale Zahl: " + nums.minimale());
         System.out.println("Maximale summe von n-1 Zahlen: " + nums.maximalSum());
-        System.out.println("Minimale summe von n-1 Zahlen: " + nums.minimalSum());
+        System.out.println("Minimale summe von n-1 Zahlen: " + nums.minimalSum()); */
+
+        //Aufgabe 3
+
+        int[] num1 = {1,3,0,0,0,0,0,0,1};
+        int[] num2 = {8,7,0,0,0,0,0,0,0};
+        GrosseZahlen number = new GrosseZahlen(num1, num2);
+
+        System.out.println("Summe: " + Arrays.toString(number.addGrosseZahlen(num1, num2)));
+
+        System.out.println("Differenz: " + Arrays.toString(number.diffGrosseZahlen(num1, num2)));
+
+        System.out.println("Multiplikation: " + Arrays.toString(number.mulGrosseZahl(num1, 100)));
+
+        System.out.println("Division: " + Arrays.toString(number.divGrosseZahl(num2, 10)));
+
     }
 }
